07edd5608a6ec63c420f98f13513ad318a3483ad
maverick-farhan <mdfarhanofficial@gmail.com>
2023-07-27 18:07:54 +0530

    All Done

--- accordion/src/script.js
+++ accordion/src/script.js
@@ -1,16 +1,12 @@
 //Accordion
 let controls = document.querySelectorAll('.accordion-control');
-let content = document.querySelectorAll('div.accordion-panel')
-content.forEach((elem)=>{
-        elem.style.transition = "clip-path 1s ease";
-        console.log(elem)
-    })
+let content = document.querySelectorAll('div.accordion-panel');
+let icons = document.querySelectorAll('span#show i');
 
     controls.forEach((control) => {
     control.addEventListener('click',(e)=>{
     e.preventDefault();
-
-    
+    console.log(control.children);
     if (control.classList.contains("active"))
     {
             control.classList.remove("active")
